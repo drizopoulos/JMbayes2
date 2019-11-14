db23117d52c181e87094970afc0885e1fab8fd28
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-11-14 13:17:47 +0100

    added rowlogsumexp (needs testing)

--- inst/include/JMbayes2cpp.h
+++ inst/include/JMbayes2cpp.h
@@ -24,8 +24,27 @@ arma::vec rowsum_by_group_fast (const vec& x, const uvec& group) {
 
 // logsumexp versions of rowsum
 // recursive version
-arma::vec rowlogsumexp_by_group_fast (const vec& x, const uvec& group) {
-  
+arma::vec rowlogsumexp_recursive_by_group (const vec& x, const uvec& group) {
+  // initiate out vector
+  vec out = x.elem(group);
+  // number of unique groups 
+  unsigned int k = group.n_elem;
+  // create new vector for indexing in the loop
+  vec group_index = group.insert_rows(0, 1);
+  // initiate loop over each group to apply recursive LSE 
+  for (int i = 1; i < k; ++i) {
+    vec x_i = x.rows(group_index.elem(i - 1), group_index.elem(i));
+    int n_i = x_i.n_elem;
+    vec x_i_sorted = sort(x_i, 'descend');
+    double Lk = x_i_sorted(0);
+    for (int j = 0; j < n_i - 1; ++i) {
+      double maxelem = xi_sorted(j + 1).insert_rows(1, Lk).max();
+      double logelem = log1p(exp(-abs(xi_sorted(j + 1) - Lk)));
+      double logsumexp_i = maxelem + logelem;
+    }
+    out(i - 1) = logsumexp_i;
+  }
+  return(out);
 }
 
 #endif
