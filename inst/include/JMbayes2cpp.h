0e7f666d2bd89052bf1f67b1537db99c0f854aca
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-11-14 12:17:36 +0100

    rowsum_svft (OLD) + initiate logsumexp version

--- inst/include/JMbayes2cpp.h
+++ inst/include/JMbayes2cpp.h
@@ -8,6 +8,24 @@
 using namespace Rcpp;
 using namespace arma;
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
 // Include Function/Class/Template declarations/definitions here to be used in multiple .cpp files
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+//rowsum_svft (OLD_VERSION)
+arma::vec rowsum_by_group_fast (const vec& x, const uvec& group) {
+  vec cum_x = cumsum(x);
+  vec out = cum_x.elem(group);
+  out.insert_rows(0, 1);
+  unsigned int n = out.n_elem;
+  out = out.rows(1, n - 1) - out.rows(0, n - 2);
+  return(out);
+}
+
+// logsumexp versions of rowsum
+// recursive version
+arma::vec rowlogsumexp_by_group_fast (const vec& x, const uvec& group) {
+  
+}
 
 #endif
