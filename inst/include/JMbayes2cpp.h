42aee007e237e275f1874d2b33f484789f8f538c
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2019-11-14 10:45:34 +0100

    inst/include + an overall header file

---
+++ inst/include/JMbayes2cpp.h
@@ -0,0 +1,13 @@
+#ifndef JMBAYES2CPP_H
+#define JMBAYES2CPP_H
+
+#include <Rcpp.h>
+#include <RcppArmadillo.h>
+// [[Rcpp::depends("RcppArmadillo")]]
+
+using namespace Rcpp;
+using namespace arma;
+
+// Include Function/Class/Template declarations/definitions here to be used in multiple .cpp files
+
+#endif
