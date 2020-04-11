55272ae46fc0893bc3278aa49961ba074f36de77
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2020-04-11 11:37:43 +0200

    fixed-interval censoring

--- Development/jm/PBC_data.R
+++ Development/jm/PBC_data.R
@@ -1,7 +1,6 @@
 pbc2$id <- factor(pbc2$id, levels = levels(pbc2$id),
                   labels = paste0("A", levels(pbc2$id)))
 pbc2$status2 <- as.numeric(pbc2$status != "alive")
-pbc2.id$status2 <- as.numeric(pbc2.id$status != "alive")
 pbc2$status3 <- as.character(pbc2$status)
 ff <- function (x) {
     out <- if (x[1L] %in% c('dead', 'transplanted')) 'dead' else
@@ -13,5 +12,5 @@ pbc2$status3 <- unname(with(pbc2, sapply(status3, function (x)
     switch(x, 'dead' = 1, 'alive' = 0, 'left' = 2, 'interval' = 3))))
 pbc2$yearsU <- as.numeric(NA)
 pbc2$yearsU[pbc2$status3 == 3] <- pbc2$years[pbc2$status3 == 3] +
-    runif(sum(pbc2$status3 == 3), 0, 4)
+    runif(sum(pbc2$status3 == 3), 0, 1)
 pbc2.id <- pbc2[!duplicated(pbc2$id), ]
