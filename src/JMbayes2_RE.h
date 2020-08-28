1990eebf993fb1acc9f023b83e7b4a38cd42a7ac
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2020-08-28 10:02:29 +0200

    updates

---
+++ src/JMbayes2_RE.h
@@ -0,0 +1,12 @@
+#ifndef JMBAYES2SURV_H
+#define JMBAYES2SURV_H
+
+#include <Rcpp.h>
+#include <RcppArmadillo.h>
+#include "JMbayes2_Funs.h"
+#include "JMbayes2_LogDens.h"
+// [[Rcpp::depends("RcppArmadillo")]]
+
+using namespace Rcpp;
+using namespace arma;
+
