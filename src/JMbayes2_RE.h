a94eb50a3c7d7587d9acad1929c5de4ac8edd313
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2020-09-17 12:06:51 +0200

    updates

--- src/JMbayes2_RE.h
+++ src/JMbayes2_RE.h
@@ -122,10 +122,48 @@ vec log_b (const field<mat> &X, const field<mat> &X_H, const field<mat> &X_h, co
 
 
 
-void update_b (field<mat> &b, mat &b_mat, const cube &chol_S, const vec &scale_sigmas, ind_RE) {
-  
-  mat proposed_b = propose_mvnorm_mat(1, L, scale_b) + b_mat;
+void update_b (field<mat> &b, mat &b_mat, const cube &chol_S, const vec &scale_b, 
+               const field<uvec> &ind_RE, mat &Wlong_H, mat &Wlong_h, mat &Wlong_H2,
+               const field<mat> &X_H, const field<mat> &X_h, const field<mat> &X_H2,
+               const field<mat> &Z_H, const field<mat> &Z_h, const field<mat> &Z_H2,
+               const field<mat> &U_H, const field<mat> &U_h, const field<mat> &U_H2,
+               const field<vec> &betas, 
+               const uvec &id_H, const uvec &id_h,
+               const field<uvec> &FunForms, const field<uvec> &FunForms_ind,
+               const bool &any_event, const bool &any_interval, 
+               const field<mat> &X, const field<mat> &Z, 
+               const field<uvec> &id, const field<mat> &y,  const vec &sigmas,
+               const vec &extra_parms, const CharacterVector &families,
+               const CharacterVector &links, const field<uvec> &ids,
+               const field<uvec> &unq_ids, const vec &W0H_bs_gammas, const vec &W0h_bs_gammas,
+               const vec &W0H2_bs_gammas, const vec &WH_gammas,
+               const vec &Wh_gammas, const vec &WH2_gammas,
+               const vec &log_Pwk, const vec &log_Pwk2,
+               const uvec &indFast_H, const uvec &which_event,
+               const uvec &which_right_event, const uvec &which_left,
+               const bool &any_interval, const uvec &which_interval,
+               const mat &L, const vec &sds 
+               ) {
+  // propose new random effects
+  mat proposed_b = propose_mvnorm_mat(1, chol_S, scale_b) + b_mat;
   field<mat> proposed_b_field = mat2field_mat(proposed_b, ind_RE); 
+  update_Wlong(Wlong_H, Wlong_h, Wlong_H2, X_H, X_h, X_H2, Z_H, Z_h, Z_H2, 
+               U_H, U_h, U_H2, betas, proposed_b_field, id_H, id_h, FunForms, FunForms_ind, 
+               any_event, any_interval);
+  vec WlongH_alphas = Wlong_H * alphas;
+  vec Wlongh_alphas(W0_h.n_rows);
+  vec WlongH2_alphas(W0_H2.n_rows);
+  if (any_event) {
+    Wlongh_alphas = Wlong_h * alphas;
+  }
+  if (any_interval) {
+    WlongH2_alphas = Wlong_H2 * alphas;
+  }
+  vec numerator_b = log_b(X, Z, betas, proposed_b_field, proposed_b, id, y, sigmas, 
+                          extra_parms, families, links, ids, unq_ids, W0H_bs_gammas, 
+                          W0h_bs_gammas, W0H2_bs_gammas, WH_gammas, 
+                          Wh_gammas, WH2_gammas, WlongH_alphas, Wlongh_alphas, 
+                          WlongH2_alphas)
   
   
   
