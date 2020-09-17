bfbc44d07d844a4e5d4a229e042d2e044119f8ab
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2020-09-17 21:02:51 +0200

    updates

--- src/mcmc_fit.cpp
+++ src/mcmc_fit.cpp
@@ -139,13 +139,10 @@ List mcmc_cpp (List model_data, List model_info, List initial_values,
   mat acceptance_sds(n_iter, n_sds, fill::zeros);
   mat res_L(n_iter, n_L, fill::zeros);
   mat acceptance_L(n_iter, n_L, fill::zeros);
-<<<<<<< Updated upstream
   cube res_b(n_b, b_mat.n_cols, n_iter, fill::zeros);
   mat acceptance_b(n_iter, n_b);
-=======
   mat res_sigmas(n_iter, n_sigmas, fill::zeros);
   mat acceptance_sigmas(n_iter, n_sigmas, fill::zeros);
->>>>>>> Stashed changes
   // scales
   vec scale_bs_gammas = create_init_scale(n_bs_gammas);
   vec scale_gammas = create_init_scale(n_gammas);
