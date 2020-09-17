ff209da0363f36ee391ab2ff4bef4fef822049e7
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2020-09-17 11:22:23 +0200

    updates

--- src/mcmc_fit.cpp
+++ src/mcmc_fit.cpp
@@ -70,16 +70,12 @@ List mcmc_cpp (List model_data, List model_info, List initial_values,
   field<uvec> x_notin_z = List2Field_uvec(as<List>(model_data["x_notin_z"]), true);
   field<uvec> unq_idL = List2Field_uvec(as<List>(model_data["unq_idL"]), true);
   field<uvec> idL_lp = List2Field_uvec(as<List>(model_data["idL_lp"]), true);
-<<<<<<< HEAD
   field<uvec> idL_lp_fast(idL_lp.n_elem);
   for (uword i = 0; i < idL_lp.n_elem; ++i) {
     idL_lp_fast.at(i) = create_fast_ind(idL_lp.at(i));
   }
   vec extra_parms = as<vec>(model_data["extra_parms"]);
-=======
   field<uvec> ind_RE = List2Field_uvec(as<List>(model_data["ind_RE"]), true);
-  //vec extra_parms = as<vec>(model_data["extra_parms"]);
->>>>>>> f0a95ef3533848de30465f14faad699a8a609797
   CharacterVector families = as<CharacterVector>(model_info["family_names"]);
   CharacterVector links = as<CharacterVector>(model_info["links"]);
   // initial values
