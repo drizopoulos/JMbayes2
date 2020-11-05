09610ed188d06c11e0ce93c5bb453d37646ce2d1
pedromafonso <p.mirandaafonso@erasmusmc.nl>
2020-11-05 12:17:14 +0100

    Update JMbayes2_Funs.h
    New function to add zero-cols and/or zero-rows to a matrix. As an alternative to J_i  %*% ...  %*% t(J_i) that we discussed yersterday.

--- src/JMbayes2_Funs.h
+++ src/JMbayes2_Funs.h
@@ -569,4 +569,22 @@ vec docall_rbindF (const field<vec> &F) { // binds a field of vectors into one v
   return V;
 }
 
+mat add_zero_colrows (const mat &M, // adds zero-rows and/or zero-cols to a matrix M
+                      const uword &nrows, // n_rows in the target matrix
+                      const uword &ncols, // n_cols in the target matrix
+                      const uvec &rows_ind,    // ind where to place the M's rows (zero-rows will be 'added' to the absent ind). the number of ind must match the M's n_rows
+                      const uvec &cols_ind) { // ind where to place the M's cols (zero-cols will be 'added' to the absent ind). the number of ind must match the M's n_cols
+  
+  mat Res(nrows, ncols, fill::zeros);
+  uword M_nrows = M.n_rows; 
+  uword M_ncols = M.n_cols;
+  
+  for(uword i = 0; i < M_nrows; i++) { // by row
+    for(uword j = 0; j < M_ncols; j++) { // by col
+      Res.at(rows_ind.at(i), cols_ind.at(j)) = M.at(i, j);
+    }
+  }
+  return Res;
+}
+
 #endif
