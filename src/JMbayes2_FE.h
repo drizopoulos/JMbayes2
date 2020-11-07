c13254532f14cf4ec14ea17f2d402325780953a2
pedromafonso <p.mirandaafonso@erasmusmc.nl>
2020-11-07 17:41:50 +0100

    Update JMbayes2_FE.h

--- src/JMbayes2_FE.h
+++ src/JMbayes2_FE.h
@@ -8,6 +8,25 @@
 using namespace Rcpp;
 using namespace arma;
 
+// To update on jm()
+  // ind_FE_nHC <- which(!seq_len(sum(nfes)) %in% ind_FE_HC)  # new
+  // indL_FE_nHC <- mapply2(function (x, ind) seq_along(x)[-ind], ind_FE, x_in_z_base) # new
+  // has_tilde_betas <- sapply(ind_FE_nHC, length) > 0
+  // indL_FE_nHC[] <- lapply(ind_FE_nHC, function (x) if (length(x)) x else 0L) # new because changed the name of the var, the remaining is the same
+
+// To update on jm()
+  // change q_dot to nfes_HC 
+
+// To update on C functions:
+  // field<vec> vec2field (const vec &betas, const field<uvec> &ind_FE) {
+  //   uword n = ind_FE.n_elem;
+  //   field<vec> out(n);
+  //   for (uword i = 0; i < n; i++) {
+  //     out.at(i) = betas.rows(ind_FE.at(i) - 1); # <- The change is here, with the "-1". The counter in ind_FE starts from 1, and not 0. I cannot do ind_FE-1, because ind_FE is a field.
+  //   }
+  //   return out;
+  // }
+
 void update_betas (field<vec> &betas, // it-th sampled fixed effects
                    mat &res_betas, // all sampled fixed effects
                    const uword &it, // current iteration
@@ -16,31 +35,38 @@ void update_betas (field<vec> &betas, // it-th sampled fixed effects
                    const mat &L, // RE corr matrix factorization (lower matrix)
                    const vec &sds, // RE SDs
                    const mat &X_dot, // X_dot matrix
-                   const field<uvec> &ind_FE, // indices for the FE present in each outcome (to save the sampled FE into a field)
-                   const uvec &ind_FE_HC, // indices for the FE present in the HC (cols in res_betas)
-                   const uvec &ind_FE_nHC, // indices for the FE NOT present in the HC (cols in res_betas)
+                   const field<uvec> &ind_FE // indices for the FE in res_betas[it,] belonging to the field betas. E.g., {{1,2,3}, {4, 5}, {6}}
+                   const uvec &ind_FE_HC, // indices for the FE present in the HC (cols in res_betas). Counts from 1.
+                   const uvec &ind_FE_nHC, // indices for the FE NOT present in the HC (cols in res_betas) Counts from 1.
+                   const field<uvec> &indL_FE_nHC, // indices for the FE NOT present in the HC per outcome (rows is each field element. the count is reset for each outcome). Counts from 1.
                    const vec &id_patt, // vector with the ids' outcome missing pattern
-                   const field<uvec> &ind_RE_patt, // indices for the RE present in each outcome missing pattern (cols in D)
-                   const field<uvec> &ind_FE_patt // indices for the FE (in HC) present in each outcome missing pattern (cols in X_dot)
+                   const field<uvec> &ind_RE_patt, // indices for the RE present in each outcome missing pattern (cols in D). Counts from 1.
+                   const field<uvec> &ind_FE_patt, // indices for the FE (in HC) present in each outcome missing pattern (cols in X_dot). Counts from 1.
+                   mat &acceptance_betas,
+                   vec &scale_betas,
+                   const uvec &has_tilde_betas,
+                   const uword &RMu_it_thld, // robins monro univariate iterations threshold to update the scale. Updates scale from it > RMu_it_thld
+                   const double &RMu_tacce // robins monro univariate target acceptance
                   )
    
    
   // The variables below could also be function's input parameters. They not change between iterations.
   uword n = b_mat.n_rows; // number of unique subjects
+  uword n_y = betas.n_elem; // number of longitudinal outcomes
   uword re_count = L.n_cols; // number of random effects
   uword patt_count = id_patt.max(); // number of unique outcome-missing patterns          
   
-  vec prior_mean_FE2 = docall_rbindF(prior_mean_FE); // bind the mean-priors from all outcomes in one vector
-  mat prior_Tau_FE2  = bdiagF(prior_Tau_FE);  // bind the Tau-priors from all outcomes in one block-matrix
+  vec joint_prior_mean_FE2 = docall_rbindF(prior_mean_FE); // bind the mean-priors from all outcomes in one vector
+  mat joint_prior_Tau_FE2  = bdiagF(prior_Tau_FE);  // bind the Tau-priors from all outcomes in one block-matrix
   
-  vec prior_mean_FE_HC = prior_mean_FE2.rows(ind_FE_HC - 1); // prior for FE in HC 
-  mat prior_Tau_FE_HC  = prior_Tau_FE2.submat(ind_FE_HC - 1, ind_FE_HC - 1);
+  vec prior_mean_FE_HC = joint_prior_mean_FE.rows(ind_FE_HC - 1); // prior for FE in HC 
+  mat prior_Tau_FE_HC  = joint_prior_Tau_FE.submat(ind_FE_HC - 1, ind_FE_HC - 1);
   
-  vec prior_mean_FE_nHC = prior_mean_FE2.rows(ind_FE_nHC - 1); // prior for FE NOT in HC 
-  mat prior_Tau_FE_nHC  = prior_Tau_FE2.submat(ind_FE_nHC - 1, ind_FE_nHC - 1);
+  vec prior_mean_FE_nHC = joint_prior_mean_FE.rows(ind_FE_nHC - 1); // prior for FE NOT in HC 
+  mat prior_Tau_FE_nHC  = joint_prior_Tau_FE.submat(ind_FE_nHC - 1, ind_FE_nHC - 1);
   
-  // For the FE in HC we update the full row on the res_betas and then update the field betas. 
-  // For the FE outside HC we do the opposite, i.e., we update per outcome in the field betas, and then update the full row in res_betas
+  // For the FE in HC we first update the full row on the res_betas and then update the field betas. 
+  // For the FE outside HC we update the field betas per outcome, and then update the full row in res_betas
 
   // FE in HC - Gibss sampling
   uword p_hc = ind_FE_HC.n_elem; // number of FE in the HC
@@ -50,12 +76,8 @@ void update_betas (field<vec> &betas, // it-th sampled fixed effects
   mat L_D = L.each_row() % sds.t(); // RE vcov matrix factorization
   field<mat> D_inv(patt_count); // all unique vcov_inv matrices accross the missing outcome patterns
 
-  if(it = 0) { 
-    vec current_FE_HC(p_hc) = docall_rbindF(betas).rows(ind_FE_nHC - 1)
-  } else { 
-    vec current_FE_HC(p_hc) = res_betas.submat(it, ind_FE_nHC - 1).t(); // skips the docall_rbindF() step
-  }
-  
+  vec betas_HC(p_hc) = docall_rbindF(betas).rows(ind_FE_HC - 1)
+
   uword patt_i;
   mat L_patt_inv
   mat X_dot_i;
@@ -70,7 +92,7 @@ void update_betas (field<vec> &betas, // it-th sampled fixed effects
     
     if (i < patt_count) { // obtain all unique vcov_inv matrices required for the sums in the posterior parameters
       
-      L_patt_inv = inv( trimatl( chol_update(L_D, ind_RE_patt.at(i)) ) );
+      L_patt_inv = inv( trimatl( chol_update(L_D, ind_RE_patt.at(i) - 1) ) );
       D_inv.at(i) =  L_patt_inv.t() * L_patt_inv;
       
     }
@@ -78,7 +100,7 @@ void update_betas (field<vec> &betas, // it-th sampled fixed effects
     X_dot_i = X_dot.submat(ind_RE_patt.at(patt_i) - 1 + i*re_count, 
                            ind_FE_patt.at(patt_i) - 1); 
     
-    u_i = b_mat.row(i).t() + X_dot_i * current_FE_HC.elem(ind_FE_patt.at(patt_i) - 1);
+    u_i = b_mat.row(i).t() + X_dot_i * betas_HC.elem(ind_FE_patt.at(patt_i) - 1);
     D_inv_i = D_inv.at(patt_i);  
     
     XD_i = X_dot_i.t() * D_inv_i;
@@ -98,56 +120,46 @@ void update_betas (field<vec> &betas, // it-th sampled fixed effects
   
   res_betas.submat(it, ind_FE_HC - 1) = (propose_mvnorm_mat(1, U_1, {1}) + mean_1).t();
   
+  // we update the FE_HC in betas after the M-H sampling
+  
   // FE not in HC - Metropolis-Hastings sampling
   
-  //////////////////////////////////////////////////////////////////////////////
-  // Draft of the required code
+  // (TEMPORARY) additional function inputs
+  const field<uvec> &L_prop_betas
   
-  // additional function inputs
-  mat &acceptance_betas
-  const field<uvec> &indF_FE_nHC // indices for the FE NOT present in the HC per outcome
-  vec &scale_betas
-  const field<uvec> &vcov_prop_betas
-  const uword &RMu_it_thld // robins monro univariate iterations threshold to update the scale
-  const double &RMu_acce // robins monro univariate target acceptance
-  const field<uvec> &ind_FE // needs to be something like {{1,2,3}, {1, 2}, {1}}. It seems to be generated on jm().
-  // other variables
-  
-  uword n_y = ; // number of outcomes with FE not in HC
+  // (TEMPORARY) other variables
   vec proposed_betas;
- 
   double numerator;
   double denominator;
   
-  for (uword i = 0; i < n_y; ++i) { // this is not correct, I need to account for the tfact that some outcome may not need this step
+  for (uword i = 0; i < n_y; ++i) {
     
-    if(it = 0) { denominator = target_log_dist(betas.at(i).rows(indF_FE_nHC.at(i) - 1)) }
-    
-    proposed_betas = rmvnorm(1, L, scale) + betas.at(i).rows(indF_FE_nHC.at(i) - 1); // use the correct function
-    
-    numerator = target_log_dist(proposed_betas); // use the correct function
+    if (has_tilde_betas.rows(it)) { // some outcomes may not have FE out of the HC
       
-    log_ratio =  numerator - denominator;
-    
-    if(log_ratio > log(runif(1))) {
-      betas.at(i).rows(indF_FE_nHC.at(i) - 1) = proposed_betas;
-      acceptance_betas.at(i, it) = 1; // check if the way toa cces acceptance betas is correct 
-      denominator = numerator;
+      if(it = 0) { denominator = target_log_dist(betas.at(i).rows(indL_FE_nHC.at(i) - 1)) }
+      
+      proposed_betas = MNORM(1, L_prop_betas, scale) + betas.at(i).rows(indL_FE_nHC.at(i) - 1); // UPDATE <-------------------------
+      
+      numerator = TARGET_LOG_DIST(proposed_betas); // UPDATE <-------------------------
+      
+      log_ratio =  numerator - denominator;
+      
+      if(std::isfinite(log_ratio) && std::exp(log_ratio) > R::runif(0.0, 1.0)) {
+        
+        betas.at(i).rows(indL_FE_nHC.at(i) - 1) = proposed_betas;
+        acceptance_betas.at(it, i) = 1;
+        denominator = numerator;
+        
+      }
+      
+      if(it > RMu_it_thld) {
+        scale_betas.rows(i) = robbins_monro(scale_betas.rows(i), acceptance_betas.at(it, i), it, RMu_tacce);
+      }
     }
-    
-    if(it > RMu_it_thld) {
-      scale_betas.at(i) = robbins_monro_univ(scale = scale_betas.at(i), // use the corrrect function
-                                           acceptance_it = acceptance_betas.at(i, it),
-                                           it = it, target_acceptance = RMu_acce)
-    }
-    
   }
   
   res_betas.submat(it, ind_FE_nHC - 1) = docall_rbindF(betas).rows(ind_FE_nHC - 1);
   
-  //////////////////////////////////////////////////////////////////////////////
-  
-  // the FE in HC were not update yet inside the field betas. We add the betas and 'again' the betas_tilde. Doing this here makes the code 'lighter' 
   betas = vec2field(res_betas.row(it), ind_FE);
   
 )
