9c9ec43e2c8f55e775d1f423b95f4e4474037cc0
pedromafonso <p.mirandaafonso@erasmusmc.nl>
2020-11-09 12:16:48 +0100

    Update JMbayes2_Funs.h
    function to sample from a multivariate normal distribution using the vcov matrix (instead of a cube)

--- src/JMbayes2_Funs.h
+++ src/JMbayes2_Funs.h
@@ -276,6 +276,15 @@ mat propose_mvnorm_mat (const int &n, const cube &S, const vec &scale) {
   return out.t();
 }
 
+mat propose_mvnorm_vec (const int &n, // number of samples
+                        const mat &U, // upper triangular Choleski factorization of the vcov matrix
+                        const double &scale) {
+  uword ncols = U.n_cols;
+  mat res(n, ncols);
+  res = scale * (rnorm_mat(n, ncols) * U);
+  return res.t(); // j-th column reports the j-th sample
+}
+
 vec mu_fun (const vec &eta, const std::string &link) {
   uword n = eta.n_rows;
   vec out(n);
