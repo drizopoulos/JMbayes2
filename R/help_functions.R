6655bff1e77c28616aacedb9733104b029b72e1b
gpapageorgiou <papageorgiou.grigorios@gmail.com>
2021-05-10 11:02:25 +0200

    ggtraceplot fix

--- R/help_functions.R
+++ R/help_functions.R
@@ -756,7 +756,7 @@ ggprepare <- function(object,
                                "tau_bs_gammas", "gammas", "alphas")) {
     parm <- match.arg(parm)
     n_chains <- object$control$n_chains
-    n_iter <- object$control$n_iter - object$control$n_burnin
+    n_iter <- (object$control$n_iter - object$control$n_burnin) / object$control$n_thin
     widedat_list <- ggextractmcmc(object$mcmc)
     widedat <- widedat_list[[1]]
     n_parms_each_fam <- widedat_list[[2]]
@@ -1217,5 +1217,40 @@ ms_prepdat <- function (timesmat, statusmat, id, starting_time, starting_state, 
            id = id, starting_time = new_times, starting_state = new_states,
            transitionmat = transitionmat[-starts, -starts], original_states = original_states[-starts],
            longmat = longmat)
+}
+
+extract_mcmc_as_inits <- function(x_mcmc, i, nams_x, nams_special, ind_RE, dim_D, has_sigmas) {
+    c(lapply(x_mcmc[!nams_x %in% nams_special], function(x, i) x[[i]][nrow(x[[i]]), , drop = TRUE], i = i), 
+      'log_sigmas' = unname(lapply(x_mcmc[nams_x %in% 'sigmas'], function(x, i, has_sigmas) ifelse(has_sigmas, log(x[[i]][nrow(x[[i]]), , drop = TRUE]), -20), i = i, has_sigmas = has_sigmas)),
+      list(betas = unname(lapply(x_mcmc[nams_x[grep('betas', nams_x)]], function(x, i) x[[i]][nrow(x[[i]]), , drop = TRUE], i = i))), 
+      list(b = mapply(function(x, y, i) x[[i]][,y , dim(x[[i]])[length(dim(x[[i]]))], drop = TRUE], 
+                      x_mcmc[nams_x %in% 'b'], ind_RE, 
+                      i = i, SIMPLIFY = FALSE, USE.NAMES = FALSE)), 
+      lapply(x_mcmc[nams_x %in% 'D'], function(x, i, dim_D) {
+          D <- matrix(NA, ncol = dim_D, nrow = dim_D)
+          D_vec <- x[[i]][nrow(x[[i]]), , drop = TRUE]
+          D[upper.tri(D, diag = TRUE)] <- D_vec[order(names(D_vec))]
+          D[lower.tri(D, diag = FALSE)] <- D[upper.tri(D, diag = FALSE)]
+          D
+      }, 
+      i = i, dim_D = dim_D)
+    )
+}
+
+extract_last_iterations <- function(x) {
+    x_mcmc <- x$mcmc
+    n_chains <- x$control$n_chains
+    nams_x <- names(x_mcmc)
+    nams_special <- c(nams_x[grep('betas', nams_x)], 'b', 'D', 'sigmas')
+    ind_RE <- x$model_data$ind_RE
+    dim_D <- ncol(x$statistics$Mean$b)
+    last_iter_x <- vector('list', n_chains)
+    has_sigmas <- as.integer(x$initial_values$log_sigmas > -20)
+    for (i in 1:n_chains) {
+        last_iter_x[[i]] <- extract_mcmc_as_inits(x_mcmc, i = i, nams_x = nams_x, 
+                                                  nams_special = nams_special, ind_RE = ind_RE, dim_D, 
+                                                  has_sigmas = has_sigmas)
+    }
+    last_iter_x
 }
 
