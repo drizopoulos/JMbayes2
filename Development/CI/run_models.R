d5ed7a652a8c798f7c04208a7e17e4942dc5b2a9
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2021-12-21 10:44:29 +0100

    updates

--- Development/CI/run_models.R
+++ Development/CI/run_models.R
@@ -93,15 +93,16 @@ CoxFit <- coxph(Surv(start, stop, event) ~ strata(CR) * (age + sex + IE),
                 data = pbc2_CR)
 
 # In the functional forms we specify that the effect of the longitudinal
-# biomarker is different after the IE status, and different per cause
+# biomarker is different before and after the IE status for death, but the
+# biomarker has no effect on the risk of transplantation
 dummy <- function (f, lvl) as.numeric(f == lvl)
 fForms <- ~ value(log(serBilir)):dummy(CR, "transplanted") +
     value(log(serBilir)):dummy(CR, "dead"):dummy(IE, 0) +
     value(log(serBilir)):dummy(CR, "dead"):dummy(IE, 1)
 
-fForms <- ~ value(log(serBilir)) + value(log(serBilir)):IE +
-    value(log(serBilir)):CR + value(log(serBilir)):IE:CR
-
+# To specify that the biomarker has no effect on the risk of transplantation, we
+# contraint the corresponding association parameter alpha via the prior (i.e.,
+# we assume prior mean zero and prior precision 20000)
 jointFit <- jm(CoxFit, lmeFit, time_var = "year", functional_forms = fForms,
                priors = list(Tau_alphas = list(diag(20000, 1), diag(1), diag(1))),
                n_iter = 15000L, n_burnin = 5000L)
@@ -122,40 +123,48 @@ newdataE_withIE$event <- 0
 # absence of the IE.
 newdataE_withoutIE <- newdataE_withIE[c(1, 3), ]
 
-# We specify the follow-up time t0 at which we want to calculate the
+# We specify the follow-up times t0 at which we want to calculate the
 # conditional causal effect, and the length of the medically-relevant time
 # window Delta_t
-t0 <- 3
+t0 <- c(3, 5, 7 , 9)
 Delta_t <- 2
 
-# Some data management steps first:
-# In the longitudinal data.frame we keep the measurements before t0
-newdataL_i <- newdataL[newdataL$year <= t0, ]
-# In the event data.frame without the IE we set the stop time at t0
-newdataE_withoutIE_i <- newdataE_withoutIE
-newdataE_withoutIE_i$stop <- t0
-# In the event data.frame with the IE we set that the IE occurs a bit after t0
-# and a bit afterward is the last time the patient was available
-newdataE_withIE_i <- newdataE_withoutIE_i
-newdataE_withIE_i$IE <- 1
+causal_effect <- matrix(0.0, length(t0), 3,
+                        dimnames = list(paste("t0 =", t0),
+                                        c("Effect", "95%_low", "95%_upp")))
 
-# We calculate the predictions using the two datasets
-newdata_withIE_i <- list(newdataL = newdataL_i, newdataE = newdataE_withIE_i)
-newdata_withoutIE_i <- list(newdataL = newdataL_i, newdataE = newdataE_withoutIE_i)
+for (i in seq_along(t0)) {
+    # Some data management steps first:
+    # In the longitudinal data.frame we keep the measurements before t0
+    newdataL_i <- newdataL[newdataL$year <= t0[i], ]
+    # In the event data.frame without the IE we set the stop time at t0
+    newdataE_withoutIE_i <- newdataE_withoutIE
+    newdataE_withoutIE_i$stop <- t0[i]
+    # In the event data.frame with the IE we set that the IE occurs a bit after t0
+    # and a bit afterward is the last time the patient was available
+    newdataE_withIE_i <- newdataE_withoutIE_i
+    newdataE_withIE_i$IE <- 1
 
-predSurv_withoutIE <- predict(jointFit, newdata = newdata_withoutIE_i,
-                           process = "event", times = t0 + Delta_t,
-                           return_mcmc = TRUE)
-predSurv_withIE <- predict(jointFit, newdata = newdata_withoutIE_i,
-                           newdata2 = newdata_withIE_i, return_mcmc = TRUE,
-                           process = "event", times = t0 + Delta_t)
+    # We calculate the predictions using the two datasets
+    newdata_withIE_i <- list(newdataL = newdataL_i, newdataE = newdataE_withIE_i)
+    newdata_withoutIE_i <- list(newdataL = newdataL_i, newdataE = newdataE_withoutIE_i)
 
-# The conditional causal effect
-predSurv_withIE$pred[2] - predSurv_withoutIE$pred[2]
+    predSurv_withoutIE <- predict(jointFit, newdata = newdata_withoutIE_i,
+                                  process = "event", times = t0[i] + Delta_t,
+                                  return_mcmc = TRUE)
+    predSurv_withIE <- predict(jointFit, newdata = newdata_withoutIE_i,
+                               newdata2 = newdata_withIE_i, return_mcmc = TRUE,
+                               process = "event", times = t0[i] + Delta_t)
 
-# 95% CI
-samples <- predSurv_withIE$mcmc[2, ] - predSurv_withoutIE$mcmc[2, ]
-quantile(samples, probs = c(0.025, 0.975))
+    # The conditional causal effect
+    causal_effect[i, 1] <- predSurv_withIE$pred[2] - predSurv_withoutIE$pred[2]
+
+    # 95% CI
+    samples <- predSurv_withIE$mcmc[2, ] - predSurv_withoutIE$mcmc[2, ]
+    causal_effect[i, 2:3] <- quantile(samples, probs = c(0.025, 0.975))
+}
+
+causal_effect
 
 #=========================================
 
