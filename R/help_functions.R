debacffb824f89f2784b3f546a70562946dab9c3
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2025-05-26 15:13:39 +0200

    updates

--- R/help_functions.R
+++ R/help_functions.R
@@ -1425,7 +1425,7 @@ create_sigma_list <- function (sigmas, ss_sigmas, idL) {
 lng_unq <- function (x) length(unique(x))
 factor2 <- function (x, ...) factor(x, levels = unique(x), ...)
 
-plot_hazard <- function (object, CI = TRUE) {
+plot_hazard <- function (object, CI = TRUE, plot = TRUE) {
     r <- range(object$model_data$Time_right)
     tt <- seq(r[1L], r[2L], len = 501)
     nstrata <- length(unique(object$model_data$strata))
@@ -1435,13 +1435,17 @@ plot_hazard <- function (object, CI = TRUE) {
     h <- exp(c(W0 %*% object$statistics$Mean$bs_gammas))
     low <- exp(c(W0 %*% object$statistics$CI_low$bs_gammas))
     upp <- exp(c(W0 %*% object$statistics$CI_upp$bs_gammas))
-    plot(r, range(low, upp), type = "n", xlab = "Time",
-         ylab = "Baseline Hazard Function")
-    if (CI) {
-        polygon(c(tt, rev(tt)), c(low, rev(upp)), col = "lightgrey",
-                border = NA)
+    if (plot) {
+        plot(r, range(low, upp), type = "n", xlab = "Time",
+             ylab = "Baseline Hazard Function")
+        if (CI) {
+            polygon(c(tt, rev(tt)), c(low, rev(upp)), col = "lightgrey",
+                    border = NA)
+        }
+        lines(tt, h, lwd = 2, col = "red")
+    } else {
+        cbind(time = tt, h = h, low = low, upp = upp)
     }
-    lines(tt, h, lwd = 2, col = "red")
 }
 
 
