7f6800b6c7c34e1a9cefd7e6c65c91a743cc6728
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2026-04-07 21:26:40 +0200

    updates

--- Development/simulate_optMod.R
+++ Development/simulate_optMod.R
@@ -1,9 +1,10 @@
 library("JMbayes2")
+library("lattice")
 source("./R/optimal_model.R")
 
 sim_fun1 <- function (n = 100) {
-    K <- 15 # number of measurements per subject
-    t_max <- 8 # maximum follow-up time
+    K <- 30 # number of measurements per subject
+    t_max <- 7 # maximum follow-up time
     # we construct a data frame with the design:
     # everyone has a baseline measurement, and then measurements at random
     # follow-up times up to t_max
@@ -15,7 +16,7 @@ sim_fun1 <- function (n = 100) {
     X <- model.matrix(~ sex * time, data = DF)
     Z <- model.matrix(~ time, data = DF)
     betas <- c(-2.2, -0.25, 0.24, -0.05) # fixed effects coefficients
-    sigma <- 0.5 # errors' standard deviation
+    sigma <- 1.5 # errors' standard deviation
     D11 <- 1.0 # variance of random intercepts
     D22 <- 0.5 # variance of random slopes
     # we simulate random effects
@@ -73,8 +74,8 @@ sim_fun1 <- function (n = 100) {
 }
 
 sim_fun2 <- function (n = 100) {
-    K <- 15 # number of measurements per subject
-    t_max <- 8 # maximum follow-up time
+    K <- 30 # number of measurements per subject
+    t_max <- 7 # maximum follow-up time
     # we construct a data frame with the design:
     # everyone has a baseline measurement, and then measurements at random
     # follow-up times up to t_max
@@ -83,13 +84,13 @@ sim_fun2 <- function (n = 100) {
                      sex = rep(gl(2, n/2, labels = c("male", "female")), each = K))
 
     # design matrices for the fixed and random effects
-    X <- model.matrix(~ sex * (time + I(time^2)), data = DF)
-    Z <- model.matrix(~ time + I(time^2), data = DF)
+    X <- model.matrix(~ sex * nsk(time, 2), data = DF)
+    Z <- model.matrix(~ nsk(time, 2), data = DF)
     betas <- c(5.2, -0.25, 0.2, 0.1, -0.2, -0.1) # fixed effects coefficients
-    sigma <- 0.5 # errors' standard deviation
-    D11 <- 0.6 # variance of random intercepts
+    sigma <- 1 # errors' standard deviation
+    D11 <- 0.7 # variance of random intercepts
     D22 <- 0.5 # variance of random slopes
-    D33 <- 0.05 # variance of quadratic random slopes
+    D33 <- 0.1 # variance of quadratic random slopes
     # we simulate random effects
     b <- cbind(rnorm(n, sd = sqrt(D11)), rnorm(n, sd = sqrt(D22)),
                rnorm(n, sd = sqrt(D33)))
@@ -101,7 +102,7 @@ sim_fun2 <- function (n = 100) {
     upp_Cens <- 5.1 # fixed Type I censoring time
     shape_wb <- 7 # shape Weibull
     alpha <- 0.15 # association coefficients
-    gammas <- c("(Intercept)" = -12, "sex" = 0.5)
+    gammas <- c("(Intercept)" = -8, "sex" = 0.5)
     W <- model.matrix(~ sex, data = DF[!duplicated(DF$id), ])
     # linear predictor for the survival model
     eta_t <- as.vector(W %*% gammas)
@@ -134,6 +135,7 @@ sim_fun2 <- function (n = 100) {
         Root <- try(uniroot(invS, interval = c(1e-05, Up), i = i)$root, TRUE)
         trueTimes[i] <- if (!inherits(Root, "try-error")) Root else 150
     }
+
     # we use fixed Type I right censoring denoting the end of the trial.
     Ctimes <- upp_Cens
     Time <- pmin(trueTimes, Ctimes)
@@ -146,8 +148,8 @@ sim_fun2 <- function (n = 100) {
 }
 
 sim_fun3 <- function (n = 100) {
-    K <- 15 # number of measurements per subject
-    t_max <- 8 # maximum follow-up time
+    K <- 30 # number of measurements per subject
+    t_max <- 7 # maximum follow-up time
     # we construct a data frame with the design:
     # everyone has a baseline measurement, and then measurements at random
     # follow-up times up to t_max
@@ -159,10 +161,10 @@ sim_fun3 <- function (n = 100) {
     X <- model.matrix(~ time + I(time^2), data = DF)
     Z <- model.matrix(~ time + I(time^2), data = DF)
     betas <- c(5.2, 0.2, 0.1) # fixed effects coefficients
-    sigma <- 0.5 # errors' standard deviation
+    sigma <- 1 # errors' standard deviation
     D11 <- 0.6 # variance of random intercepts
-    D22 <- 0.5 # variance of random slopes
-    D33 <- 0.05 # variance of quadratic random slopes
+    D22 <- 0.3 # variance of random slopes
+    D33 <- 0.1 # variance of quadratic random slopes
     # we simulate random effects
     b <- cbind(rnorm(n, sd = sqrt(D11)), rnorm(n, sd = sqrt(D22)),
                rnorm(n, sd = sqrt(D33)))
@@ -174,7 +176,7 @@ sim_fun3 <- function (n = 100) {
     upp_Cens <- 5.1 # fixed Type I censoring time
     shape_wb <- 7 # shape Weibull
     alpha <- 0.15 # association coefficients
-    gammas <- c("(Intercept)" = -12, "sex" = 0.5)
+    gammas <- c("(Intercept)" = -10, "sex" = 0.5)
     W <- model.matrix(~ sex, data = DF[!duplicated(DF$id), ])
     # linear predictor for the survival model
     eta_t <- as.vector(W %*% gammas)
@@ -207,6 +209,7 @@ sim_fun3 <- function (n = 100) {
         Root <- try(uniroot(invS, interval = c(1e-05, Up), i = i)$root, TRUE)
         trueTimes[i] <- if (!inherits(Root, "try-error")) Root else 150
     }
+
     # we use fixed Type I right censoring denoting the end of the trial.
     Ctimes <- upp_Cens
     Time <- pmin(trueTimes, Ctimes)
@@ -233,24 +236,29 @@ training_id <- training[!duplicated(training$id), ]
 CoxFit <- coxph(Surv(Time, event) ~ sex, data = training_id)
 
 fm1 <- lme(y ~ time * sex, data = training, random = ~ time | id)
-fm2 <- lme(y ~ sex * nsk(time, 3), data = training,
-           random = list(id = pdDiag(~ nsk(time, 3))))
-fm3 <- lme(y ~ poly(time, 2), data = training,
+fm2 <- lme(y ~ sex * ns(time, k = c(2, 4), B = c(0, 7)), data = training,
+           random = list(id = pdDiag(~ ns(time, k = c(2, 4), B = c(0, 7)))))
+fm3 <- lme(y ~ poly(time, 2) * sex, data = training,
            random = list(id = pdDiag(~ poly(time, 2))))
+fm4 <- lme(y ~ poly(time, 3), data = training,
+           random = list(id = pdDiag(~ poly(time, 3))))
 
 jointFit1 <- jm(CoxFit, fm1, time_var = "time")
 jointFit2 <- jm(CoxFit, fm2, time_var = "time")
 jointFit3 <- jm(CoxFit, fm3, time_var = "time")
-Models <- list(jointFit1, jointFit2, jointFit3)
+jointFit4 <- jm(CoxFit, fm4, time_var = "time")
+Models <- list(jointFit1, jointFit2, jointFit3, jointFit4)
 
-T0 <- 5
+T0 <- 2
+Dt <- 1
 Data <- testing[ave(testing$time, testing$id, FUN = max) > T0, ]
 Data$Time <- T0; Data$event <- 0
 Data_before <- Data[Data$time <= T0, ]
-Data_after <- Data[Data$time > T0, ]
+Data_after <- Data[Data$time > T0 & Data$time <= T0 + Dt, ]
 preds <- lapply(Models, predict, newdata = Data_before, newdata2 = Data_after)
 Preds <- do.call('cbind', lapply(preds, function (x) x$newdata$preds[[1L]]))
-Obs <- Data$y[Data$time <= T0]
+Preds <- Preds[Data_before$time > T0 - Dt, ]
+Obs <- Data$y[Data$time <= T0 & Data$time > T0 - Dt]
 best_model_before <- which.min(colMeans((Preds - Obs)^2))
 ff <- function (x) {
     nx <- length(x)
@@ -259,35 +267,36 @@ ff <- function (x) {
 ind_first <- as.logical(with(Data_after, ave(time, id, FUN = ff)))
 Preds_after <- do.call('cbind', lapply(preds, function (x) x$newdata2$preds[[1L]]))
 Obs_after <- Data_after$y
+Data_before <- Data_before[Data_before$time > T0 - Dt, ]
 best_model_after <- which.min(colMeans((Preds_after - Obs_after)^2))
 
 oracle <-
     apply(rowsum((Preds_after - Obs_after)^2, Data_after$id, reorder = FALSE),
           1L, which.min)
 id <- match(Data_after$id, unique(Data_after$id))
-# oracle predictions (per id best model for after predictions)
+# oracle predictions (per id best model for the after predictions)
 mean((Preds_after[cbind(seq_along(id), oracle[id])] - Obs_after)^2)
-
-ni <- table(match(Data_before$id, unique(Data_before$id)))
-mse_id <- c(1 / ni) * rowsum((Preds - Obs)^2, Data_before$id, reorder = FALSE)
+mse_id <- rowsum((Preds - Obs)^2, Data_before$id, reorder = FALSE) /
+    c(table(match(Data_before$id, unique(Data_before$id))))
 model_id <- apply(mse_id, 1L, which.min)
 id <- match(Data_after$id, unique(Data_after$id))
-# MSEs after
-colMeans((Preds_after - Obs_after)^2)
-# MSE after for best model from testing1
+# MSE after for best model from before measurements
+colMeans((Preds_after - Obs_after)^2)[best_model_before]
+# MSE after for best model from after measurements
 colMeans((Preds_after - Obs_after)^2)[best_model_after]
 # MSE best model per id
 mean((Preds_after[cbind(seq_along(id), model_id[id])] - Obs_after)^2)
-# MSE weighted average mses per id
+# MSE weighted average MSEs per id
 weights <- t(apply(1 / mse_id, 1L, function (x) exp(x) / sum(exp(x))))
-mean((rowSums(weights[id, ] * Preds_after) - Obs_after)^2, na.rm = TRUE)
+mean((rowSums(weights[id, ] * Preds_after) - Obs_after)^2)
 
 
-ni <- table(match(Data_before$id, unique(Data_before$id)))
-mse_id_before <- c(1 / ni) * rowsum((Preds - Obs)^2, Data_before$id, reorder = FALSE)
-ni <- table(match(Data_after$id, unique(Data_after$id)))
-mse_id_after <- c(1 / ni) * rowsum((Preds_after - Obs_after)^2, Data_after$id, reorder = FALSE)
 
+
+ni <- c(table(match(Data_before$id, unique(Data_before$id))))
+mse_id_before <- rowsum((Preds - Obs)^2, Data_before$id, reorder = FALSE) / ni
+ni <- c(table(match(Data_after$id, unique(Data_after$id))))
+mse_id_after <- rowsum((Preds_after - Obs_after)^2, Data_after$id, reorder = FALSE) / ni
 
 cor(cbind(mse_id_before, mse_id_after))[1:3, 4:6]
 
@@ -296,8 +305,25 @@ JMbayes::IndvPred_lme(fm2, newdata = Data_before[1:4, ], timeVar = "time", times
 JMbayes::IndvPred_lme(fm3, newdata = Data_before[1:4, ], timeVar = "time", times = 2:8)$predicted_y
 
 
+Data_before$Preds1 <- Preds[, 1L]
+Data_before$Preds2 <- Preds[, 2L]
+Data_before$Preds3 <- Preds[, 3L]
+xyplot(y + Preds1 + Preds2 + Preds3 ~ time | factor(id), data = Data_before,
+       type = c("p", "l", "l", "l"), distribute.type = TRUE, cex = 0.8, pch = 8,
+       auto.key = TRUE, layout = c(5, 5))
 
 
+
+Data_after$Preds1 <- Preds_after[, 1L]
+Data_after$Preds2 <- Preds_after[, 2L]
+Data_after$Preds3 <- Preds_after[, 3L]
+
+xyplot(y + Preds1 + Preds2 + Preds3 ~ time | factor(id), data = Data_after,
+       type = c("p", "l", "l", "l"), distribute.type = TRUE, cex = 0.8, pch = 8,
+       auto.key = TRUE, layout = c(5, 5))
+
+Data_before[Data_before$id == 84, ]
+Data_after[Data_after$id == 84, ]
 
 
 ##
